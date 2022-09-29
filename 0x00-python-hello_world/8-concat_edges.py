695dbcee306fed03285200d00834b7de772fc744
Abuonzone <auduabubakar1992@gmail.com>
2022-09-29 12:30:25 +0000

    Hello world: Task 8 update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(f"{str[40:66]} {str[107:111]} {str[:7]}")
-#print("{} {} {}".format(str[39:66], str[107:111], str[:7]))
+print(f"{str[40:66]}")
+print(f"{str[107:111] {str[:7]}")
